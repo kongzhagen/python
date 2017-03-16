e1e648527f76eb1f7c75d9f3bfef5d978b6276a3
kongzhagen <290528767@qq.com>
2017-03-16 16:46:13 +0800

    天天美剧

--- spiders/scrapyProjects/movie/movie/settings.py
+++ spiders/scrapyProjects/movie/movie/settings.py
@@ -4,7 +4,7 @@
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
-#
+# #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
